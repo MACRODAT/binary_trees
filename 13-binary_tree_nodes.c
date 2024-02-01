a9395bfceeaf5847cc1ec7916c2529040a38c216
MACRODAT <macrodatservices@gmail.com>
2024-02-01 02:55:43 +0100

    Changes to the file

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,11 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * binary_tree_nodes - counts the nodes
  *
- * @tree: pointer to the root node of the tree to count the number of nodes
- * Return: If tree is NULL, the function must return 0
- * A NULL pointer is not a node
+ * @tree: pointer
+ * Return: nodes
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)

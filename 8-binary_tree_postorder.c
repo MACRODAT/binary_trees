330ea696fda268441ebffd072d589ef8cedca486
MACRODAT <macrodatservices@gmail.com>
2024-02-01 02:20:40 +0100

    Changes to the file

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,11 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through binary tree using post-order traversal
- * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node.
- * The value in the node must be passed as a parameter to this function.
- * If tree or func is NULL, do nothing
+ * binary_tree_postorder - post-order traversal
+ * @tree: pointer
+ * @func: pointer
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,24 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
+ * binary_tree_height - heu
  *
- * @tree: pointer to the root node of the tree to measure the height.
- * Return: 0 If tree is NULL, or the height
+ * @tree: pointer
+ * Return: the height
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t hlf = 0, hrg = 0;
+	size_t h_left = 0, h_right = 0;
 
-	if (!tree)
-		return (0);
-
+	if (binary_tree_is_leaf(tree))
+		return (1);
 	if (tree->left)
-		hlf = 1 + binary_tree_height(tree->left);
+		h_left = binary_tree_height(tree) + 1;
 	if (tree->right)
-		hrg = 1 + binary_tree_height(tree->right);
-
-	if (hlf > hrg)
-		return (hlf);
-	return (hrg);
+		h_right = binary_tree_height(tree) + 1;
+	if (h_left > h_right)
+		return (h_left);
+	return (h_right);
 }

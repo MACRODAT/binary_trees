f8b7f19706cfcfa638308fb13156cc898ba28c75
MACRODAT <macrodatservices@gmail.com>
2024-02-01 02:12:33 +0100

    Changes to the file

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,12 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ * binary_tree_preorder - pre-order traversal
  *
- * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node.
- * The value in the node must be passed as a parameter to this function.
- * If tree or func is NULL, do nothing
+ * @tree: pointer
+ * @func: pointer to a function
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))

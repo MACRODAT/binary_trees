103142814e9e3ab185bfbcff595d979519386a35
MACRODAT <macrodatservices@gmail.com>
2024-02-01 03:24:00 +0100

    Changes to the file

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,22 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - finds the sibling of a node
+ * binary_tree_sibling - sibling of a node
  *
- * @node: pointer to the node to find the sibling
- * Return: pointer to the sibling node
+ * @node: ptr
+ * Return: pointer
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent;
+	binary_tree_t *prt;
 
 	if (!node || !node->parent)
 		return (NULL);
 
-	parent = node->parent;
-	if (node == parent->left)
-		return (parent->right);
+	prt = node->parent;
+	if (node == prt->left)
+		return (prt->right);
 
-	return (parent->left);
+	return (prt->left);
 }

622d96059ac3f20cebe6a03c2fb304a5b84e49b7
MACRODAT <macrodatservices@gmail.com>
2024-02-01 03:06:12 +0100

    Changes to the file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,11 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure the balance factor
+ * binary_tree_height_balance - balance measure
+ * @tree: pointer
  *
  * Return: height
- * If tree is NULL, return 0
  */
 
 size_t binary_tree_height_balance(const binary_tree_t *tree)
@@ -26,11 +25,9 @@ size_t binary_tree_height_balance(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- *
- * @tree: pointer to the root node of the tree
+ * binary_tree_balance - measure balance factor
+ * @tree: pointer
  * Return: balance factor
- * If tree is NULL, return 0
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {

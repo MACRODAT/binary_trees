b58e5f411f3dee9ba3b1b2ba718e112bbe8a30ad
MACRODAT <macrodatservices@gmail.com>
2024-02-02 04:08:08 +0100

    Changes to the file

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,20 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_aux - tree
+ * binary_tree_level_cur - tree
  * @tree: ptr
  * Return: Tre
  */
-size_t	binary_tree_height_aux(const binary_tree_t *tree)
+size_t	binary_tree_level_cur(const binary_tree_t *tree)
 {
 	size_t hleft = 0, hright = 0;
 
 	if (!tree)
 		return (0);
 	if (tree->left)
-		hleft = 1 + binary_tree_height_aux(tree->left);
+		hleft = 1 + binary_tree_level_cur(tree->left);
 	if (tree->right)
-		hright = 1 + binary_tree_height_aux(tree->right);
+		hright = 1 + binary_tree_level_cur(tree->right);
 
 	if (hleft > hright)
 		return (hleft);
@@ -51,16 +51,16 @@ void print_level_order(const binary_tree_t *tree, int level, void (*func)(int))
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	int height = 0;
-	int len = 1;
+	int i = 1;
 
 	if (!tree || !func)
 		return;
 
-	height = binary_tree_height_aux(tree) + 1;
+	height = binary_tree_level_cur(tree) + 1;
 
-	while (len <= height)
+	while (i <= height)
 	{
-		print_level_order(tree, len, func);
-		len++;
+		print_level_order(tree, i, func);
+		i++;
 	}
 }

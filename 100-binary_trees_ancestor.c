01d685b9842c8a5d3c619bde7e09cff98156db2f
MACRODAT <macrodatservices@gmail.com>
2024-02-02 04:00:24 +0100

    Changes to the file

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,11 +10,11 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 					const binary_tree_t *second)
 {
-	const binary_tree_t *f_anc, *s_anc;
+	const binary_tree_t *my_ans, *s_anc;
 
-	for (f_anc = first; f_anc; f_anc = f_anc->parent)
-		for (s_anc = second; s_anc; s_anc = s_anc->parent)
-			if (f_anc == s_anc)
-				return ((binary_tree_t *)f_anc);
+	for (my_ans = first; my_ans != NULL; my_ans = my_ans->parent)
+		for (s_anc = second; s_anc != NULL; s_anc = s_anc->parent)
+			if (my_ans == s_anc)
+				return ((binary_tree_t *)my_ans);
 	return (NULL);
 }
